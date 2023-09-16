a9cb6b3f1e59fea9beeea6ba4e3c6d95bd0a4630
Rafiw Idrissu <dev.rafiu@gmail.com>
2023-09-16 12:08:22 -0700

    install and configure tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    color: {
+      foreground: "#fff",
+      "landing-page": "#2F1F14",
+      nav: "#6F4436",
+      footer: " #291600",
+      reviews: "#655546",
+      discover: "#DBBCA1",
+      "discover-foreground": "#DBBCA1",
+    },
+  },
+  plugins: [],
+};
